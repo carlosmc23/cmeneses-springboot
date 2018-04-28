79d4395e8d8f7104c3f040a0f2096d5629fb8d26
Carlos Meneses <kenshinmc23@gmail.com>
2018-04-27 22:04:52 -0400

    REST APIS

--- src/main/java/com/avanticaspring/demo/service/TeacherService.java
+++ src/main/java/com/avanticaspring/demo/service/TeacherService.java
@@ -10,13 +10,47 @@ import java.util.List;
 @Service
 public class TeacherService {
 
+    private static final List<Teacher> listTeachers= new ArrayList<>();
 
-    public List<Teacher> getAllTeachers(){
+    public TeacherService() {
+        listTeachers.add(new Teacher(0,"carlos"));
+        listTeachers.add(new Teacher(1,"jorge"));
+        listTeachers.add(new Teacher(2,"rudy"));
+        listTeachers.add(new Teacher(3,"noemi"));
+    }
+
+    /*public List<Teacher> getAllTeachers(){
         List<Teacher> listTeachers= new ArrayList<>();
         listTeachers.add(new Teacher(1,"carlos"));
         listTeachers.add(new Teacher(2,"jorge"));
         listTeachers.add(new Teacher(3,"rudy"));
+        listTeachers.add(new Teacher(4,"noemi"));
         return listTeachers;
+    }*/
+
+    //CRUD
+    //List teachers
+    public List<Teacher> getAllTeachers(){
+        return listTeachers;
+    }
+    //Add new teacher
+    public void addNewTeacher(Teacher newteacher){
+        //instert new teacher into DB
+        listTeachers.add(newteacher);
+    }
+    //delete teacher by id
+    public void deleteTeacher(int id){
+        //delete a
+        listTeachers.remove(id);
+    }
+    //Get a teacher By Id
+    public Teacher getTeacherById(int id){
+        return listTeachers.get(id);
+    }
+    //delete teacher by id
+    public void deleteTeacher(Teacher delTeacher){
+        //delete a
+        listTeachers.remove(delTeacher.getId());
     }
 
 }
--- src/main/java/com/avanticaspring/demo/web/TeacherController.java
+++ src/main/java/com/avanticaspring/demo/web/TeacherController.java
@@ -2,22 +2,50 @@ package com.avanticaspring.demo.web;
 
 import com.avanticaspring.demo.domain.Teacher;
 import com.avanticaspring.demo.service.TeacherService;
+import io.swagger.annotations.Api;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 
 @RestController
 @RequestMapping("/teachers")
-public class TeacherController {
+@Api(value="onlinestore", description="Operations pertaining to products in Online Store")
 
+public class TeacherController {
+    //dependecy injection
     @Autowired
-    TeacherService teacherService;
+    private TeacherService teacherService;
+    //get all teachers
     @RequestMapping(method = RequestMethod.GET)
-    public List<Teacher> getTeacher(){
+    public List<Teacher> getAllTeachers(){
         return teacherService.getAllTeachers();
+    }
+    //Insert new teacher
+    @RequestMapping(method = RequestMethod.POST)
+    public void addNewTeacher(@RequestBody Teacher newTeacher){
+        System.out.println("the name of new teacher is: "+newTeacher);
+        teacherService.addNewTeacher(newTeacher);
+    }
+    //Delete teacher localhost:9090/teachers/2
+    @RequestMapping(path = "/{id}", method = RequestMethod.DELETE)
+    public void deleteTeacher(@PathVariable int id){
+        System.out.println("the id teacher delete is: "+ id);
+        teacherService.deleteTeacher(id);
+    }
+    //delete teacher por el requestbody
+    @RequestMapping(method = RequestMethod.DELETE)
+    public void deleteTeacherBody(@RequestBody Teacher delTeacher){
+        System.out.println("the name of teacher deleted is: "+delTeacher);
+        // revisar teacherService.deleteTeacher(delTeacher);
+        teacherService.deleteTeacher((int)delTeacher.getId());
+    }
+
+    //get teacher by id
+    @RequestMapping(path = "/{id}", method = RequestMethod.GET)
+    public Teacher getTeacherById(@PathVariable int id) {
+        System.out.println("the id teacher is: " + id);
+        return teacherService.getTeacherById(id);
     }
 }
